0252c9689eb50b529e8a49ae3f6747521ea51edb
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-22 13:03:07 -0700

    pagination

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" 0-simple_helper_function file
+"""
+from typing import Tuple
+
+def index_range(page: int, page_size: int) -> Tuple:
+   page_start = 0
+   page_end = 0
+   
+   for i in range(page):
+       page_start = page_end
+       page_end += page_size
+
+   return (page_start, page_end)
