fcc01311ecd37c9493deacd60176dfaa1a6074e1
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-22 14:59:37 -0700

    pagination

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -5,11 +5,7 @@ from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    page_start = 0
-    page_end = 0
-
-    for i in range(page):
-        page_start = page_end
-        page_end += page_size
+    page_start = (page - 1) * page_size
+    page_end = page_start + page_size
 
     return (page_start, page_end)
