593f9e3a91068116a1dcf3d393f205c0fe5fdba2
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-10-31 19:18:10 -0700

    flask

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""A Basic Flask app.
+"""
+from flask_babel import Babel
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+class Config:
+    """Represents a Flask Babel configuration.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@babel.localeselector
+def get_locale():
+    """dddd """
+    locale = request.args.get('locale')
+    if locale:
+        return locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def hello() -> str:
+    ''' render index.html template '''
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
