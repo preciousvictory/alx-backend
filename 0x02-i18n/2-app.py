16c50b8ff046b95ef4a88c2a82ca031b80d94919
Victory Abiodun-Omoniyi <victoryexcel2018@gmail.com>
2023-11-01 02:57:55 -0700

    flask

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -22,6 +22,7 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale():
+    '''get_locale function'''
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
